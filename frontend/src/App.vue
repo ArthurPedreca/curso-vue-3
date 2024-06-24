da12db1ab8482f269fe173cf8471e71079291075
Arthur Pedreca <arthurpedreca@gmail.com>
2024-06-24 13:26:41 -0300

    ta com erro essa porra

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,67 +1,24 @@
-<template>
-
-  <template v-if="showName">
-    Mostrar o nome
-  </template>
-
-  <div :class="teste">teste</div>
-
-  <img :src="image" alt="">
-
-  <ul>
-    <template v-for="(user, key) in users">
-
-      <li v-if="user.is_admin == 1">
-        {{ key }}: {{ user.userName }}
-      </li>
-      
-    </template>
-  </ul>
-</template>
+<template></template>
 
 <script>
+import axios from 'axios';
 export default {
-  data() {
-    return {
-      image:"https://picsum.photos/200/300",
-      teste:"class",
-      showName:false,
-      user: { name: "arthur", age: 10 },
-      users: [
-        {
-          id: 1,
-          userName: "Arthur",
-          age: 25,
-          is_admin: 1,
-        },
-        {
-          id: 2,
-          userName: "Rogridão",
-          age: 17,
-          is_admin: 0,
-        },
-        {
-          id: 3,
-          userName: "Marco",
-          age: 16,
-          is_admin: 0,
-        },
-        {
-          id: 4,
-          userName: "Raul",
-          age: 18,
-          is_admin: 0,
-        },
-        {
-          id: 5,
-          userName: "Mateus",
-          age: 20,
-          is_admin: 1,
-        },
-      ],
-    };
+  data(){
+    return{
+      users:[]
+    }
   },
-};
+
+  async mounted(){
+    try{
+      const {data} = await axios.get('http://localhost:8000/api/users');
+      console.log(data)
+    }catch(error){
+      console.error(error)
+    }
+  },
+
+}
 </script>
 
 <style>
