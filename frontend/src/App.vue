6f6ce1499d2a80f9a58a0b415c97250157626513
Arthur Pedreca <arthurpedreca@gmail.com>
2024-06-28 13:17:27 -0300

    v-if; v-else; v-else-if; v-show (display none)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,24 +1,32 @@
 <template>
+  <hr />
 
   <h2>Lista de users</h2>
 
   <ul>
-    <li v-for="user in users" :key="user.id">{{user.lastname}}</li>
+    <li v-for="user in users" :key="user.id">
+      {{ user.lastname }}
+      <button v-if="user.is_admin">Admin</button>
+      <button v-else>not admin</button>
+    </li>
   </ul>
 </template>
 
 <script>
 import dados from "./dados.json";
+import Header from "@/components/Header.vue";
 export default {
+  components: { Header },
   data() {
     return {
       users: [],
+      showHeader: true,
     };
   },
 
- mounted(){
-  this.users = dados
- }
+  mounted() {
+    this.users = dados;
+  },
 };
 </script>
 
